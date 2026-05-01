a568f60109f84f758cec2e15958466d4f9cac425
Komal <rastogikomal57@gmail.com>
2026-05-02 00:13:31 +0530

    Daily push

--- DAY19.java
+++ DAY19.java
@@ -19,3 +19,35 @@ class Solution {
         return ans;
     }
 }
+
+//498. Diagonal Traverse
+
+class Solution2 {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length, n=mat[0].length;
+        if(m==0) return new int[0];
+        int[] ans = new int[m*n];
+        int k=0;
+        for(int d=0; d<m+n-1; d++){
+            if(d%2==0){ //move up
+                int row = Math.min(d, m-1);
+                int col = d-row;
+
+                while(row >= 0 && col < n){
+                    ans[k++] = mat[row][col];
+                    row--; col++;
+                }
+            }else{ //move down
+                int col = Math.min(d, n-1);
+                int row = d - col;
+
+                while(row < m && col >= 0){
+                    ans[k++] = mat[row][col];
+                    row++; col--;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
