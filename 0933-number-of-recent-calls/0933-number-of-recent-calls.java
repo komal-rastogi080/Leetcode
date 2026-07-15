fa0ed02bf6b10df6f392ef3879b0ea17dc052bd7
Komal Rastogi <166051719+komal-rastogi080@users.noreply.github.com>
2026-07-15 10:25:34 +0530

    Time: 34 ms (7.01%), Space: 50.8 MB (94.41%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,21 @@
+class RecentCounter {
+    Queue<Integer> q;
+    public RecentCounter() {
+        q = new PriorityQueue<>();
+    }
+    
+    public int ping(int t) {
+        q.add(t);
+        int up = t-3000;
+        while(q.peek()<up) {
+            q.poll();
+        }
+        return q.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
